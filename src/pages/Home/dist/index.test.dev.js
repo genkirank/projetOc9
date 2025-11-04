4d30f696bceba7b89d487e703810a3f3323e1f9f
Raphal Luis <raphluis11@gmail.com>
2025-11-04 11:05:31 +0100

    .

---
+++ src/pages/Home/dist/index.test.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var _react = require("@testing-library/react");
+
+var _index = _interopRequireDefault(require("./index"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+describe("When a page is created", function () {
+  it("a list of events is displayed", function () {// to implement
+  });
+  it("a list a people is displayed", function () {// to implement
+  });
+  it("a footer is displayed", function () {// to implement
+  });
+  it("an event card, with the last event, is displayed", function () {// to implement
+  });
+});
