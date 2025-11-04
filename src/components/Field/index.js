c32eb28f34b03c2711e134bac089b872d8975b36
Raphal Luis <raphluis11@gmail.com>
2025-11-04 11:02:41 +0100

    Rajout du placeholder pour voir apparaître (votre message) dans la zone de texte

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -22,7 +22,7 @@ const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder ,onChan
       );
       break;
     case FIELD_TYPES.TEXTAREA:
-      component = <textarea name={name} data-testid="field-testid"
+      component = <textarea name={name} data-testid="field-testid" placeholder={placeholder}
       onChange={onChange}
       />;
       break;
