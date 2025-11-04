dcf8399431af7111fa1ed9f6863d3f0c21f3b125
Raphal Luis <raphluis11@gmail.com>
2025-11-04 11:04:39 +0100

    Suppression de la vérification car elle est déjà dans le test du form

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,31 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "./index";
 
-describe("When Form is created", () => {
-  it("a list of fields card is displayed", async () => {
-    render(<Home />);
-    await screen.findByText("Email");
-    await screen.findByText("Nom");
-    await screen.findByText("Prénom");
-    await screen.findByText("Personel / Entreprise");
-  });
-
-  describe("and a click is triggered on the submit button", () => {
-    it("the success message is displayed", async () => {
-      render(<Home />);
-      fireEvent(
-        await screen.findByText("Envoyer"),
-        new MouseEvent("click", {
-          cancelable: true,
-          bubbles: true,
-        })
-      );
-      await screen.findByText("En cours");
-      await screen.findByText("Message envoyé !");
-    });
-  });
-
-});
 
 
 describe("When a page is created", () => {
